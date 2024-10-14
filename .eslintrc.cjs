b848fbfdac4323f9a7867d8bcf9739faf55422b3
Brach Burdick <developers@die-hard.io>
2024-10-13 22:57:10 -0400

    fix lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,22 +13,18 @@ const config = {
     "plugin:@typescript-eslint/stylistic-type-checked"
   ],
   "rules": {
-    "@typescript-eslint/array-type": "off",
-    "@typescript-eslint/consistent-type-definitions": "off",
-    "@typescript-eslint/consistent-type-imports": [
-      "warn",
-      {
-        "prefer": "type-imports",
-        "fixStyle": "inline-type-imports"
-      }
-    ],
-    "@typescript-eslint/no-unused-vars": [
-      "warn",
-      {
-        "argsIgnorePattern": "^_"
-      }
-    ],
-    "@typescript-eslint/require-await": "off",
+    "@typescript-eslint/array-type": "warn",
+    '@typescript-eslint/prefer-nullish-coalescing': 'warn',
+    "@typescript-eslint/no-unused-vars": "warn",
+    "@typescript-eslint/no-unsafe-return": "warn",
+    "@typescript-eslint/no-unsafe-assignment": "warn",
+    "@typescript-eslint/no-unsafe-argument": "warn",
+    "@typescript-eslint/no-floating-promises": "warn",
+    "@typescript-eslint/await-thenable": "warn",
+    "prefer-const": "warn",
+    "@typescript-eslint/consistent-type-definitions": "warn",
+    "@typescript-eslint/consistent-type-imports": "warn",
+    "@typescript-eslint/require-await": "warn",
     "@typescript-eslint/no-misused-promises": [
       "error",
       {
--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,3 @@
-import { postRouter } from "~/server/api/routers/post";
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
 import { essentiaRouter } from "./routers/essentia";
 import { songsRouter } from "./routers/songs";
@@ -12,7 +11,6 @@ import { type inferRouterInputs, type inferRouterOutputs } from "@trpc/server";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
   essentia: essentiaRouter,
   songs: songsRouter,
 });
